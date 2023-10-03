056f6e1c51f637aef977ec3efe20917597618f71
QuinnFeng <quinnfeng26276@gmail.com>
2023-10-03 13:56:48 -0400

    completed carousel effect with vanilla JS and CSS

--- js/index.js
+++ js/index.js
@@ -1,13 +1,15 @@
 const tabs = document.querySelectorAll("[data-target]");
 const mountainsTab = document.querySelector("#mountainsTab");
 const active = "active";
-const mountains = document.querySelectorAll(".mountain");
+const carouselIndicators = document.querySelector(".carousel-indicators");
+const indicators = document.querySelectorAll("[data-slide-to]");
+let currentCarouselIndex = 0;
 
 mountainsTab.addEventListener("click", function (event) {
   const clickedTab = event.target;
-  if (clickedTab.dataset.target === undefined) {
-    return;
-  }
+  // if (clickedTab.dataset.target === undefined) {
+  //   return;
+  // }
   tabs.forEach((tab) => {
     const target = tab.dataset.target;
     const mountainEle = document.getElementById(target);
@@ -25,3 +27,38 @@ function toggleAccordion({ target }) {
 for (let button of accordionButtons) {
   button.addEventListener("click", toggleAccordion);
 }
+
+carouselIndicators.addEventListener("click", function (event) {
+  const clickedTarget = event.target;
+  indicators.forEach((indicator) =>
+    toggleActiveCarouselItem(indicator, clickedTarget)
+  );
+});
+
+function toggleActiveCarouselItem(indicator, targetElem) {
+  const target = indicator.dataset.slideTo;
+  currentCarouselIndex = +target;
+  const carouselItem = document.getElementById(`carousel-item-${target}`);
+  const isActive = indicator === targetElem;
+  indicator.classList.toggle(active, isActive);
+  carouselItem.classList.toggle(active, isActive);
+}
+
+const indicatorsArray = [...indicators];
+function toggleCarouselItem() {
+  let currentIndicator = indicatorsArray[currentCarouselIndex];
+  const carouselItem = document.getElementById(
+    `carousel-item-${currentCarouselIndex}`
+  );
+  currentIndicator.classList.toggle(active, true);
+  carouselItem.classList.toggle(active, true);
+  currentCarouselIndex = (currentCarouselIndex + 1) % indicatorsArray.length;
+
+  setTimeout(() => {
+    currentIndicator.classList.toggle(active, false);
+    carouselItem.classList.toggle(active, false);
+    // Update current and next indicators for the next cycle
+  }, 5000);
+}
+
+setInterval(toggleCarouselItem, 5000);
